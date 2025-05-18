be93bd80f377601be925da92b2a75cd94f7f2ad9
abhi <ugabhi@proton.me>
2025-05-18 09:37:38 +0530

    chore: git config update

--- users/abhi/home.nix
+++ users/abhi/home.nix
@@ -1,9 +1,9 @@
 {
   userSettings,
   config,
+  pkgs,
   ...
-}: 
-{
+}: {
   imports = [
     ./secrets
     ./lib
@@ -21,6 +21,66 @@
     enable = true;
     userName = userSettings.name;
     userEmail = userSettings.email;
+    signing = {
+      signByDefault = true;
+      key = "FFF41332AF327F6B01C58C1EC773B58665274E3E";
+    };
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+    };
+    aliases = {
+      c = "commit -m 'Refactoring'";
+      cm = "commit -m";
+      ga = "git add";
+      gp = "git push";
+      gs = "git status";
+      gaa = "git add *";
+      gcm = "git commit -m";
+    };
+    ignores = [
+      "*~"
+      "*.swp"
+      "debug/"
+      "target/"
+      "**/*.rs.bk"
+      "*.pdb"
+      "*.aux"
+      "*.dvi"
+      "*.fdb_latexmk"
+      "*.fls"
+      "*.log"
+      "*.synctex"
+      "*.lot"
+      "*.toc"
+      "*.out"
+      "*.synctex.gz"
+      "#*.org#"
+      ".#*"
+      "ltximg/"
+      "ltximg/*"
+    ];
+  };
+
+  programs.gpg = {
+    enable = true;
+    mutableKeys = true; # allows you to import/export keys manually
+    mutableTrust = true; # allows setting trust interactively
+    settings = {
+      use-agent = true;
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentry = {
+      package = pkgs.pinentry-curses; # or `pinentry-tty` if you're super minimal
+    };
+    enableSshSupport = false; # or true if you want to use GPG for SSH
+    extraConfig = ''
+      allow-loopback-pinentry
+    '';
   };
 
   home.file = {
